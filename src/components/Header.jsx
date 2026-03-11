c482ea7507d74ada7f62e050b161dcffef27b521
fabioferrari00 <fabioferrari.ff00@gmail.com>
2026-03-11 12:39:35 +0100

    working header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
 
 const Header = () => {
   return (
     <header>
-      Ciao
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-6">
+            <img src="../src/assets/logoMio-Photoroom.png" alt="ciao" className='header-logo' />
+            <span className="logo-title">Fabio Ferrari - Web Developer</span>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
