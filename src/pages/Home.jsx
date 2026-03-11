7a7dfd13cba7a42555b09026de78eb3a79f0efaf
fabioferrari00 <fabioferrari.ff00@gmail.com>
2026-03-11 14:29:46 +0100

    working homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,16 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <img className='mia-foto my-5' src="../src/assets/miafoto.png" alt="" />
+          </div>
+          <div className="col-6 mini-bio d-flex">
+            Mi chiamo Fabio, sono nato a Napoli il 06 gennaio 2000. Sono una persona motivata, con buone capacità di adattamento e di lavoro in team, e il mio obiettivo è crescere professionalmente acquisendo esperienza concreta nel settore dello sviluppo web. Durante i miei percorsi ho acquisito competenze in HTML, CSS, JavaScript, TypeScript, React, Node.js e MySQL.
+          </div>
+        </div>
+      </div>
     </>
   )
 }
